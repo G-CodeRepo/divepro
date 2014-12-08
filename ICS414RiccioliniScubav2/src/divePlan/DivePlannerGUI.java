82aafa82e2a3b0cee2ceccdf07cf9fef85366fa0
Victor <Victor@MyOtherBrain>
2014-12-07 14:49:40 -1000

    Added buttons and some integration between the back end and front end.

--- ICS414RiccioliniScubav2/src/divePlan/DivePlannerGUI.java
+++ ICS414RiccioliniScubav2/src/divePlan/DivePlannerGUI.java
@@ -1,4 +1,8 @@
 package divePlan;
+import java.util.*;
+
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.DateTime;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Button;
@@ -9,6 +13,9 @@ import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.wb.swt.SWTResourceManager;
 import org.eclipse.swt.widgets.Text;
+
+import com.ibm.icu.text.DateFormat;
+import com.ibm.icu.text.SimpleDateFormat;
 
 
 @SuppressWarnings("unused")
@@ -24,17 +31,26 @@ public class DivePlannerGUI {
 	private Button btnHistory;
 	private Button btnHelp;
 	private Button btnNewDive;
+	private Button btnNewDiveSubmit;
 	
-	private Text dateDay;
-	private Text dateMonth;
-	private Text dateYear;
-	private Text timeHour;
-	private Text timeMin;
-	private Text depth;
-	private Text timeUnderMin;
-	private Text surfaceIntervalMin;
+	private Button btnSelectDepth;
+	private Button btnSelectTimeUnder;
+	//private Button btnHistorySubmit;
 	
-	private Text diveNum;
+	//private Text dateDay;
+	//private Text dateMonth;
+	//private Text dateYear;
+	//private Text timeHour;
+	//private Text timeMin;
+	//private Text depth;
+	//private Text timeUnderMin;
+	//private Text surfaceIntervalMin;
+	
+	private Combo depth;
+	private Combo timeUnder;
+	private Combo surfaceInterval;
+	
+	//private Text diveNum;
 	
 	private Label lblWelcome;
 	private Label lblDisclaimer;
@@ -43,13 +59,13 @@ public class DivePlannerGUI {
 	private Label lblDisclaimerBody3;
 	
 	private Label lblDate;
-	private Label lblTime;
+	//private Label lblTime;
 	private Label lblDepth;
 	private Label lblTimeUnder;
 	private Label lblSurfaceInterval;
-	private Label lblDateDiv;
-	private Label lblDateDiv2;
-	private Label lblTimeDiv;
+	//private Label lblDateDiv;
+	//private Label lblDateDiv2;
+	//private Label lblTimeDiv;
 	private Label lblFt;
 	private Label lblMin;
 	private Label lblMin2;
@@ -61,13 +77,27 @@ public class DivePlannerGUI {
 	private Label lblTutorialBody4;
 	private Label lblTutorialBody5;
 	private Label lblHistoryHeader;
+	private Label lblNoDives; 
+	private Label lblPrevDiveDate;
+	private Label lblPrevDiveTime;
+	private Label lblPrevDiveDepth;
+	private Label lblPrevDiveTimeUnder;
+	private Label lblPrevDiveSurfaceInterval;
+	
+	private DateTime dateField;
+	
+	private DateFormat dateFormat;
+	private Date date;
 	
 	private boolean homeDisplay;
 	private boolean newDiveDisplay;
 	private boolean historyDisplay;
 	private boolean helpDisplay;
 	
-
+	private DivePlanner dives;
+	
+	private int indexTimeUnder;
+	
 	/**
 	 * Launch the application.
 	 * @param args
@@ -144,24 +174,97 @@ public class DivePlannerGUI {
 					btnNewDive.setEnabled(false);
 					btnNewDive.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
 					
+					final DiveTable valuesTable = new DiveTable();
+					String validDepths[] = new String[12];
+				    
+					int depthValues[] = valuesTable.getValidDepths();
+				    for(int i = 0; i < validDepths.length; i++)
+				    {
+				    	validDepths[i] = depthValues[i] + "";
+				    }
+				    
+				    btnSelectDepth = new Button(DivePlannerHome, SWT.NONE);
+					btnSelectDepth.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent s) {
+							try {
+								LinkedHashMap<Integer, int[]> bottomTimes= valuesTable.getValidBottomTimes();
+								final int intBottomTimes[] = bottomTimes.get(Integer.parseInt((depth.getText())));
+								String strBottomTimes[] = new String[intBottomTimes.length];
+								
+								for(int i = 0; i < strBottomTimes.length; i++)
+								{
+									strBottomTimes[i] = intBottomTimes[i] + "";
+								}
+								
+								timeUnder.setItems(strBottomTimes);
+								timeUnder.setVisible(true);
+								timeUnder.setText(timeUnder.getItem(0));
+								lblMin.setVisible(true);
+								
+								
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+					
+					btnSelectDepth.setBounds(300, 145, 100, 40);
+					btnSelectDepth.setText("Select Depth");
+					
+					btnSelectTimeUnder = new Button(DivePlannerHome, SWT.NONE);
+					btnSelectTimeUnder.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent s) {
+							try {
+								LinkedHashMap<Integer, int[]> bottomTimes= valuesTable.getValidBottomTimes();
+								final int intBottomTimes[] = bottomTimes.get(Integer.parseInt((depth.getText())));
+								String strBottomTimes[] = new String[intBottomTimes.length];
+								
+								for(int i = 0; i < strBottomTimes.length; i++)
+								{
+									strBottomTimes[i] = intBottomTimes[i] + "";
+								}
+								
+								for(int i = 0; i < strBottomTimes.length; i++)
+								{
+									if(strBottomTimes[i] == timeUnder.getText())
+									{
+										indexTimeUnder = i;
+									}	
+								}
+								
+								
+								
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+					
+					btnSelectTimeUnder.setBounds(300, 215, 150, 40);
+					btnSelectTimeUnder.setText("Select Time Under");
+				    
+				    
+					
 					lblDate = new Label(DivePlannerHome, SWT.NONE);
 					lblDate.setFont(SWTResourceManager.getFont("Segoe UI Light", 9, SWT.BOLD));
 					lblDate.setBounds(10, 82, 70, 20);
 					lblDate.setText("Date:");
 					
-					lblTime = new Label(DivePlannerHome, SWT.NONE);
+					/*lblTime = new Label(DivePlannerHome, SWT.NONE);
 					lblTime.setFont(SWTResourceManager.getFont("Segoe UI Light", 9, SWT.BOLD));
 					lblTime.setText("Time:");
-					lblTime.setBounds(10, 129, 70, 20);
+					lblTime.setBounds(10, 129, 70, 20);*/
 					
 					lblDepth = new Label(DivePlannerHome, SWT.NONE);
 					lblDepth.setFont(SWTResourceManager.getFont("Segoe UI Light", 9, SWT.BOLD));
-					lblDepth.setBounds(10, 180, 70, 20);
+					lblDepth.setBounds(10, 152, 70, 20);
 					lblDepth.setText("Depth:");
 					
 					lblTimeUnder = new Label(DivePlannerHome, SWT.NONE);
 					lblTimeUnder.setFont(SWTResourceManager.getFont("Segoe UI Light", 9, SWT.BOLD));
-					lblTimeUnder.setBounds(10, 223, 85, 20);
+					lblTimeUnder.setBounds(10, 220, 85, 20);
 					lblTimeUnder.setText("Time Under:");
 					
 					lblSurfaceInterval = new Label(DivePlannerHome, SWT.NONE);
@@ -169,61 +272,94 @@ public class DivePlannerGUI {
 					lblSurfaceInterval.setBounds(10, 280, 114, 20);
 					lblSurfaceInterval.setText("Surface Interval:");
 					
-					dateDay = new Text(DivePlannerHome, SWT.BORDER);
-					dateDay.setBounds(171, 79, 35, 26);
+					dateField = new DateTime(DivePlannerHome, SWT.BORDER);
+					dateField.setBounds(171, 80, 102, 28);
+			
+					//dateDay = new Text(DivePlannerHome, SWT.BORDER);
+					//dateDay.setBounds(171, 79, 35, 26);
 					
-					lblDateDiv = new Label(DivePlannerHome, SWT.NONE);
-					lblDateDiv.setBounds(212, 82, 15, 26);
-					lblDateDiv.setText("/");
+					//lblDateDiv = new Label(DivePlannerHome, SWT.NONE);
+					//lblDateDiv.setBounds(212, 82, 15, 26);
+					//lblDateDiv.setText("/");
 					
-					dateMonth = new Text(DivePlannerHome, SWT.BORDER);
-					dateMonth.setBounds(227, 79, 35, 26);
+					//dateMonth = new Text(DivePlannerHome, SWT.BORDER);
+					//dateMonth.setBounds(227, 79, 35, 26);
 					
-					lblDateDiv2 = new Label(DivePlannerHome, SWT.NONE);
-					lblDateDiv2.setBounds(266, 82, 15, 20);
-					lblDateDiv2.setText("/");
+					//lblDateDiv2 = new Label(DivePlannerHome, SWT.NONE);
+					//lblDateDiv2.setBounds(266, 82, 15, 20);
+					//lblDateDiv2.setText("/");
 					
-					dateYear = new Text(DivePlannerHome, SWT.BORDER);
-					dateYear.setBounds(275, 79, 50, 26);
+					//dateYear = new Text(DivePlannerHome, SWT.BORDER);
+					//dateYear.setBounds(275, 79, 50, 26);
 					
-					timeHour = new Text(DivePlannerHome, SWT.BORDER);
-					timeHour.setBounds(171, 126, 43, 26);
+					//timeHour = new Text(DivePlannerHome, SWT.BORDER);
+					//timeHour.setBounds(171, 126, 43, 26);
 					
-					lblTimeDiv = new Label(DivePlannerHome, SWT.NONE);
-					lblTimeDiv.setBounds(222, 129, 15, 20);
-					lblTimeDiv.setText(":");
+					//lblTimeDiv = new Label(DivePlannerHome, SWT.NONE);
+					//lblTimeDiv.setBounds(222, 129, 15, 20);
+					//lblTimeDiv.setText(":");
 					
-					timeMin = new Text(DivePlannerHome, SWT.BORDER);
-					timeMin.setBounds(238, 126, 43, 26);
+					//timeMin = new Text(DivePlannerHome, SWT.BORDER);
+					//timeMin.setBounds(238, 126, 43, 26);
 					
-					depth = new Text(DivePlannerHome, SWT.BORDER);
-					depth.setBounds(171, 177, 78, 26);
+					//depth = new Text(DivePlannerHome, SWT.BORDER);
+					//depth.setBounds(171, 177, 78, 26);
+					
+					depth = new Combo(DivePlannerHome, SWT.NONE);
+					depth.setBounds(171, 152, 78, 26);
+					depth.setItems(validDepths);
+					depth.setText(depth.getItem(0));
+					
+					timeUnder = new Combo(DivePlannerHome, SWT.NONE);
+					timeUnder.setBounds(171, 220, 78, 26);
+					timeUnder.setVisible(false);
 					
 					lblFt = new Label(DivePlannerHome, SWT.NONE);
-					lblFt.setBounds(255, 180, 70, 20);
+					lblFt.setBounds(255, 152, 70, 20);
 					lblFt.setText("Ft");
 					
-					timeUnderMin = new Text(DivePlannerHome, SWT.BORDER);
-					timeUnderMin.setBounds(171, 220, 78, 26);
+					//timeUnderMin = new Text(DivePlannerHome, SWT.BORDER);
+					//timeUnderMin.setBounds(171, 220, 78, 26);
 					
-					surfaceIntervalMin = new Text(DivePlannerHome, SWT.BORDER);
-					surfaceIntervalMin.setBounds(171, 280, 78, 26);
+					//surfaceIntervalMin = new Text(DivePlannerHome, SWT.BORDER);
+					//surfaceIntervalMin.setBounds(171, 280, 78, 26);
 					
 					lblMin = new Label(DivePlannerHome, SWT.NONE);
-					lblMin.setBounds(255, 223, 70, 20);
+					lblMin.setBounds(255, 220, 30, 20);
 					lblMin.setText("min");
+					lblMin.setVisible(false);
 					
 					lblMin2 = new Label(DivePlannerHome, SWT.NONE);
-					lblMin2.setBounds(255, 286, 70, 20);
+					lblMin2.setBounds(255, 280, 30, 20);
 					lblMin2.setText("min");
 					
 					lblNewDiveHeader = new Label(DivePlannerHome, SWT.NONE);
 					lblNewDiveHeader.setFont(SWTResourceManager.getFont("Segoe Print", 11, SWT.NORMAL));
 					lblNewDiveHeader.setBounds(10, 21, 410, 41);
 					lblNewDiveHeader.setText("Please complete the following fields:");
+					
+					btnNewDiveSubmit = new Button(DivePlannerHome, SWT.NONE);
+					btnNewDiveSubmit.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent s) {
+							try {
+								
+								dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+							    date = new Date();
+								
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+					
+					btnNewDiveSubmit.setBounds(300, 280, 110, 40);
+					btnNewDiveSubmit.setText("Add Dive");
+					btnNewDiveSubmit.setEnabled(false);
 					//DivePlannerHome.close();
 					//windowNewDive = new DivePlannerNewDive();
 					//windowNewDive.open();
+				
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -255,20 +391,20 @@ public class DivePlannerGUI {
 					TutorialBody.setText("Hello and welcome to the tutorial!");
 					
 					lblTutorialBody2 = new Label(DivePlannerHome, SWT.NONE);
-					lblTutorialBody2.setBounds(20, 121, 532, 20);
+					lblTutorialBody2.setBounds(20, 120, 532, 20);
 					lblTutorialBody2.setText("1. To add a dive, click on the new dive button and enter in the required info.");
 					
 					lblTutorialBody3 = new Label(DivePlannerHome, SWT.NONE);
-					lblTutorialBody3.setBounds(20, 159, 499, 20);
-					lblTutorialBody3.setText("2. To get data from previously recorded dives, click on the History button ");
+					lblTutorialBody3.setBounds(20, 145, 532, 20);
+					lblTutorialBody3.setText("Keep in mind that text displayed in red means that the field is not accepted.");
 					
 					lblTutorialBody4 = new Label(DivePlannerHome, SWT.NONE);
-					lblTutorialBody4.setBounds(30, 185, 509, 20);
-					lblTutorialBody4.setText("and enter the dive number desired. For example, type in \"1\" for dive 1, ");
+					lblTutorialBody4.setBounds(20, 170, 509, 20);
+					lblTutorialBody4.setText("If text is displayed in red, simply select another number.");
 					
 					lblTutorialBody5 = new Label(DivePlannerHome, SWT.NONE);
-					lblTutorialBody5.setBounds(29, 212, 199, 20);
-					lblTutorialBody5.setText("\"2\" for dive 2, and so on.");
+					lblTutorialBody5.setBounds(20, 210, 500, 20);
+					lblTutorialBody5.setText("2. To review the previous dive, simply press on the History button.");
 					
 					//DivePlannerHome.close();
 					//windowHelp = new DivePlannerHelp();
@@ -278,7 +414,7 @@ public class DivePlannerGUI {
 				}
 			}
 		});
-		btnHelp.setBounds(147, 353, 116, 73);
+		btnHelp.setBounds(149, 353, 116, 73);
 		btnHelp.setText("Help");
 		
 		btnHistory = new Button(DivePlannerHome, SWT.NONE);
@@ -297,12 +433,72 @@ public class DivePlannerGUI {
 					lblHistoryHeader = new Label(DivePlannerHome, SWT.NONE);
 					lblHistoryHeader.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_FOREGROUND));
 					lblHistoryHeader.setFont(SWTResourceManager.getFont("Segoe Print", 10, SWT.NORMAL));
-					lblHistoryHeader.setText("Please specify a dive by giving a number (1, 2, 3, etc.):");
-					lblHistoryHeader.setBounds(10, 10, 525, 44);
+					lblHistoryHeader.setText("Previous Dive:");
+					lblHistoryHeader.setBounds(10, 10, 150, 44);
 					
-					diveNum = new Text(DivePlannerHome, SWT.BORDER);
-					diveNum.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_FOREGROUND));
-					diveNum.setBounds(10, 60, 78, 26);
+					lblNoDives = new Label(DivePlannerHome, SWT.NONE);
+					lblNoDives.setBounds(10, 50, 200, 20);
+					lblNoDives.setText("No dives have been recorded.");
+					lblNoDives.setVisible(false);
+					
+					try {
+						
+						lblPrevDiveDate = new Label(DivePlannerHome, SWT.NONE);
+						lblPrevDiveDate.setBounds(10, 80, 200, 20);
+						
+						lblPrevDiveTime = new Label(DivePlannerHome, SWT.NONE);
+						lblPrevDiveTime.setBounds(10, 110, 250, 20);
+						
+						lblPrevDiveDepth = new Label(DivePlannerHome, SWT.NONE);
+						lblPrevDiveDepth.setBounds(10, 140, 250, 20);
+						
+						lblPrevDiveTimeUnder = new Label(DivePlannerHome, SWT.NONE);
+						lblPrevDiveTimeUnder.setBounds(10, 220, 250, 20);
+						
+						lblPrevDiveSurfaceInterval = new Label(DivePlannerHome, SWT.NONE);
+						lblPrevDiveSurfaceInterval.setBounds(10, 300, 250, 20);
+						
+						LinkedList<Dive> prevDives = dives.getDivePlan();
+						
+						
+						lblPrevDiveDate.setText("Date:\t\t" + dateFormat.format(date));
+					
+						lblPrevDiveTime.setText("Time:\t\t" + dateFormat.format(date));
+						
+						
+						lblPrevDiveDepth.setText("Depth:\t\t\t" +prevDives.get(prevDives.size() - 1).getDepth());
+						
+						
+						lblPrevDiveTimeUnder.setText("Time Under:\t\t" +prevDives.get(prevDives.size() - 1).getTotalBottomTime());
+						
+						lblPrevDiveSurfaceInterval.setText("Surface Interval:\t\t" +prevDives.get(prevDives.size() - 1).getSurfaceInterval());
+					
+					}catch(IllegalStateException e){
+						lblNoDives.setVisible(true);
+					}catch(NullPointerException e){
+						lblNoDives.setVisible(true);
+					}
+					
+					//diveNum = new Text(DivePlannerHome, SWT.BORDER);
+					//diveNum.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_FOREGROUND));
+					//diveNum.setBounds(10, 60, 78, 26);
+					
+					/*btnHistorySubmit = new Button(DivePlannerHome, SWT.NONE);
+					btnHistorySubmit.addSelectionListener(new SelectionAdapter() {
+						@Override
+						public void widgetSelected(SelectionEvent s) {
+							try {
+								
+								
+								
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						}
+					});
+					
+					btnHistorySubmit.setBounds(10, 60, 110, 40);
+					btnHistorySubmit.setText("Load Dive");*/
 					//DivePlannerHome.close();
 					//windowHistory = new DivePlannerHistory();
 					//windowHistory.open();
@@ -368,30 +564,45 @@ public class DivePlannerGUI {
 		
 		if(historyDisplay){
 			lblHistoryHeader.dispose();
-			diveNum.dispose();
+			lblNoDives.dispose();
+			lblPrevDiveDate.dispose();
+			lblPrevDiveTime.dispose();
+			lblPrevDiveDepth.dispose();
+			lblPrevDiveTimeUnder.dispose();
+			lblPrevDiveSurfaceInterval.dispose();
+			//diveNum.dispose();
+			//btnHistorySubmit.dispose();
 		}
 		
 		if(newDiveDisplay){
-			dateDay.dispose();
-			dateMonth.dispose();
-			dateYear.dispose();
-			timeHour.dispose();
-			timeMin.dispose();
-			depth.dispose();
-			timeUnderMin.dispose();
-			surfaceIntervalMin.dispose();
+			//dateDay.dispose();
+			//dateMonth.dispose();
+			//dateYear.dispose();
+			dateField.dispose();
+			//timeField.dispose();
+			//timeHour.dispose();
+			//timeMin.dispose();
+			//depth.dispose();
+			//timeUnderMin.dispose();
+			//surfaceIntervalMin.dispose();
 			lblDate.dispose();
-			lblTime.dispose();
+			//lblTime.dispose();
 			lblDepth.dispose();
 			lblTimeUnder.dispose();
 			lblSurfaceInterval.dispose();
-			lblDateDiv.dispose();
-			lblDateDiv2.dispose();
-			lblTimeDiv.dispose();
+			depth.dispose();
+			timeUnder.dispose();
+			//lblDateDiv.dispose();
+			//lblDateDiv2.dispose();
+			//lblTimeDiv.dispose();
 			lblFt.dispose();
 			lblMin.dispose();
 			lblMin2.dispose();
 			lblNewDiveHeader.dispose();
+			btnSelectDepth.dispose();
+			btnSelectTimeUnder.dispose();
+			//btnSelectTimeUnder.dispose();
+			btnNewDiveSubmit.dispose();
 		}
 			
 		if(helpDisplay){
